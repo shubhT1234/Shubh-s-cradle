ad88dfab0b99e8b86a35f59a327ad25f79717638
shubhT1234 <74590038+shubhT1234@users.noreply.github.com>
2021-01-02 12:56:30 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,28 @@
+class Bob {
+    constructor(x,y, radius){    
+    var options ={
+        isStatic: false,
+        restitution: 0.9,
+        friction: 1.0,
+        density: 1.2
+     }
+     this.x =x;
+     this.y=y;
+     this.radius=radius;
+     this.image=loadImage("bob.png");
+     this.body = Bodies.circle(this.x,this.y,(this.radius)/2, options);
+     World.add(world, this.body);
+  }
+
+  display(){
+    var pos = this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    rectMode(CENTER);
+    strokeWeight(3)
+    stroke("black");
+    fill("purple");
+    image(this.image,-20,0,this.radius)
+    pop();
+  }
+};
